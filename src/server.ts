9e995fe6f325df755ead428ce2670d0071f0cd09
Rafinhaa <rafinhaa@me.com>
2023-05-22 19:43:00 -0300

    fix: Habilitar server para conexões externas

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import { env } from "./env";
 
 const start = async () => {
   try {
-    await app.listen({ port: env.PORT });
+    await app.listen({ port: env.PORT, host: "0.0.0.0" });
   } catch (err) {
     app.log.error(err);
     process.exit(1);
